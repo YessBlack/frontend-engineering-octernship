d2073e0b2a0c7611a7cd7bba8c9fbcf85fb8e49e
Angelica Garcia <anyegar36@gmail.com>
2023-03-22 21:52:19 -0500

    Add routes

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Form } from './components/Form/Form';
+import banner from './assets/img/banner.svg';
+import lupa from './assets/img/lupa.png';
+import { Routes, Route } from 'react-router-dom';
+import { Result } from './components/Result/Result';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <Routes>
+        <Route path="/" element={<Form imgBanner={banner} imgLogo={lupa} />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
     </div>
   );
 }
